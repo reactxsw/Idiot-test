a60f21bc013af98c01a46c9e713e5d941413ef9e
Anapah <70201574+reactxsw@users.noreply.github.com>
2021-12-25 02:42:58 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace idiot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Yes_button = new System.Windows.Forms.Button();
+            this.No_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Yes_button
+            // 
+            this.Yes_button.Location = new System.Drawing.Point(62, 243);
+            this.Yes_button.Name = "Yes_button";
+            this.Yes_button.Size = new System.Drawing.Size(154, 55);
+            this.Yes_button.TabIndex = 0;
+            this.Yes_button.Text = "Yes";
+            this.Yes_button.UseVisualStyleBackColor = true;
+            this.Yes_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // No_button
+            // 
+            this.No_button.Location = new System.Drawing.Point(268, 243);
+            this.No_button.Name = "No_button";
+            this.No_button.Size = new System.Drawing.Size(148, 55);
+            this.No_button.TabIndex = 1;
+            this.No_button.Text = "No";
+            this.No_button.UseVisualStyleBackColor = true;
+            this.No_button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(91, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(311, 50);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Are you an idiot ?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(165, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 128);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "☻";
+            // 
+            // Form1
+            // 
+            this.AccessibleDescription = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(483, 322);
+            this.Controls.Add(this.No_button);
+            this.Controls.Add(this.Yes_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+ 
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Idiot test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Yes_button;
+        private System.Windows.Forms.Button No_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
